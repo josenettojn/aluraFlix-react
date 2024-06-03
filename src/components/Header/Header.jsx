7812ead4f163f159841a815179659a06c29824da
José Neto <joseneto1500@gmail.com>
2024-06-03 14:34:33 -0300

    style: :art: adjust style header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,26 @@
 import { Link } from 'react-router-dom'
 import styles from './Header.module.css'
+import Container from '../Container/Container'
 
 const Header = () => {
   return (
-    <header className={styles.header}>
-      <img src="/images/logo.png" alt="logo da AluraFlix" />
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">home</Link>
-          </li>
-          <li>
-            <Link to="/new">novo vídeo</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+      <div className={styles.headerContainer}>
+        <Container>
+          <header className={styles.header}>
+            <img src="/images/logo.png" alt="logo da AluraFlix" />
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">home</Link>
+                </li>
+                <li>
+                  <Link to="/new">novo vídeo</Link>
+                </li>
+              </ul>
+            </nav>
+          </header>
+        </Container>
+      </div>
   )
 }
 
