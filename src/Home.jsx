6461b31a26a2141a3494aed563f2fa5da8e1d34b
José Neto <joseneto1500@gmail.com>
2024-06-03 13:05:01 -0300

    feat: :sparkles: created main banner

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,11 @@
+import Banner from './components/Banner/Banner'
 import Header from './components/Header/Header'
 
 function Home() {
   return (
     <>
       <Header />
+      <Banner />
     </>
   )
 }
---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,19 @@
+import styles from './Banner.module.css'
+
+const Banner = () => {
+  return (
+    <div className={styles.banner} style={{ backgroundImage: 'url(/images/banner.png)' }}>
+      <div className={styles.content}>
+        <div className={styles.text}>
+          <button>front end</button>
+          <h1>SEO com React</h1>
+          <p>{`Eu to aqui pra nesse vídeo dizer que a gente vai aprender a começar uma app inspirada no desenho Pokémon com Nextjs e React, ver algumas dicas sobre performance e de quebra conhecer uma plataforma sensacional pra fazer deploy que é a Vercel. Tudo em 22 minutos nesse vídeo feito com todo o carinho do mundo construindo uma "Pokedex"!`}</p>
+        </div>
+
+        <img src="/images/banner.png" alt="banner" />
+      </div>
+    </div>
+  )
+}
+
+export default Banner
