9f81cf55add54acebfe9e679126f88033d51f7b5
José Neto <joseneto1500@gmail.com>
2024-06-03 15:55:12 -0300

    feat: :sparkles: create section footer

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,6 @@
 import Banner from './components/Banner/Banner'
 import Category from './components/Category/Category'
+import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 
 function Home() {
@@ -8,6 +9,7 @@ function Home() {
       <Header />
       <Banner />
       <Category />
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css'
+
+const Footer = () => {
+  return (
+    <Footer className={styles.footer}>
+      <img src="/images/logo.png" alt="logo da AluraFlix no footer" />
+    </Footer>
+  )
+}
+
+export default Footer
