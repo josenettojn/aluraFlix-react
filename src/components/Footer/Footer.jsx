7b8415611063010cc7ee3f668ce2be9315eeddc0
José Neto <joseneto1500@gmail>
2024-06-16 16:45:35 -0300

    feat: :heavy_plus_sign: add json server

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,9 +2,9 @@ import styles from './Footer.module.css'
 
 const Footer = () => {
   return (
-    <Footer className={styles.footer}>
+    <footer className={styles.footer}>
       <img src="/images/logo.png" alt="logo da AluraFlix no footer" />
-    </Footer>
+    </footer>
   )
 }
 
