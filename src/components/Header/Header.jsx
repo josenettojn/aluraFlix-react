68c8ef0d3fb0b6657c3689e299893627773d5146
José Neto <joseneto1500@gmail>
2024-06-18 14:20:55 -0300

    feat: :sparkles: add link to home in logo the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,27 +1,29 @@
-import { Link } from 'react-router-dom'
-import styles from './Header.module.css'
-import Container from '../Container/Container'
+import { Link } from "react-router-dom";
+import styles from "./Header.module.css";
+import Container from "../Container/Container";
 
 const Header = () => {
   return (
-      <div className={styles.headerContainer}>
-        <Container>
-          <header className={styles.header}>
+    <div className={styles.headerContainer}>
+      <Container>
+        <header className={styles.header}>
+          <Link to="/">
             <img src="/images/logo.png" alt="logo da AluraFlix" />
-            <nav>
-              <ul>
-                <li>
-                  <Link to="/">home</Link>
-                </li>
-                <li>
-                  <Link to="/new">novo vídeo</Link>
-                </li>
-              </ul>
-            </nav>
-          </header>
-        </Container>
-      </div>
-  )
-}
+          </Link>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">home</Link>
+              </li>
+              <li>
+                <Link to="/new">novo vídeo</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+      </Container>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
